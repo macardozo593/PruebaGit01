f568dbc8f29ff5461c03f495a543e42e29e65986
macardozo593 <macardozo@huenei.com>
2025-10-21 15:51:25 -0300

    Primer cambio despues del proyecto

---
+++ Controllers/Prueba01Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaGit01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Prueba01Controller : ControllerBase
+    {
+    }
+}
