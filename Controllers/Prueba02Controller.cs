1902ba0ad5fccfe33ee61519a317caeb17121e78
macardozo593 <macardozo@huenei.com>
2025-10-22 12:19:20 -0300

    Update Prueba02Controller.cs
    Modificado desde github

--- Controllers/Prueba02Controller.cs
+++ Controllers/Prueba02Controller.cs
@@ -12,7 +12,7 @@ namespace PruebaGit01.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value_01", "value_02_MCC_Modifiado desde github" };
         }
 
         // GET api/<Prueba02Controller>/5
